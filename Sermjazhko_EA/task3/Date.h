a586780acf55977a073b8aa060c0f1dbb0a1de3b
Sermjazhko <43463670+Sermjazhko@users.noreply.github.com>
2019-04-03 22:20:05 +0300

    Task 4

--- Sermjazhko_EA/task3/Date.h
+++ Sermjazhko_EA/task3/Date.h
@@ -12,9 +12,9 @@ public:
   Date(int _year, int _month, int _day); //конструктор инициализации
   Date(const Date &object); //конструктор копирования
   Date& operator=(const Date &object); // оператор присваивания
-  bool check_date(Date &object); //проверка на наличие даты
-  int time_index(const Date &object); // показывает, где курсор времени
-  int temperature_index(const Date &object); // показывает, где курсор температуры
+  int get_year(const Date &object);
+  int get_month(const Date &object);
+  int get_day(const Date &object);
   friend ostream& operator<< (ostream &out, const Date &object); // вывод
   friend istream& operator>> (std::istream &in, Date &object); // ввод
   ~Date() // деструктор
@@ -47,64 +47,17 @@ Date& Date::operator=(const Date &object)
   index_temperature = object.index_temperature;
   return *this;
 }
-bool Date::check_date(Date &object)
+int Date::get_year(const Date &object)
 {
-  int const N = 256;
-  int index = 0;
-  int index_string = 0;
-  string date;
-  int  size_array;
-  ifstream fin("Thermometer.txt");
-  char ArrayWord[N] = { "" };
-  if ((object.day / 10) != 0)
-    date += ((object.day / 10) + '0');
-  date += ((object.day % 10) + '0');
-  if ((object.month / 10) != 0)
-    date += ((object.month / 10) + '0');
-  date += ((object.month % 10) + '0');
-  date += ((object.year / 1000) + '0');
-  date += ((object.year % 1000) / 100 + '0');
-  date += ((object.year % 100) / 10 + '0');
-  date += ((object.year % 10) + '0');
-  while (!fin.eof())
-  {
-    fin.getline(ArrayWord, N);
-    size_array = strlen(ArrayWord);
-    index_string += size_array + 2;
-    if (ArrayWord[6] == date[0])
-    {
-      for (int j = 6, i = 0; j < size_array; j++, i++)
-      {
-        if (ArrayWord[j] == '.')
-          j++;
-        if (ArrayWord[j] == date[i])
-        {
-          index++;
-          if (index == (size_array - 8))
-          {
-            object.index_time = index_string;
-            fin.getline(ArrayWord, N);
-            object.index_temperature = index_string + strlen(ArrayWord) + 2;
-            fin.close();
-            return true;
-          }
-        }
-        else
-          index = 0;
-      }
-    }
-  }
-  index_string = 0;
-  fin.close();
-  return false;
+  return object.year;
 }
-int Date::time_index(const Date &object)
+int Date::get_month(const Date &object)
 {
-  return object.index_time;
+  return object.month;
 }
-int Date::temperature_index(const Date &object)
+int Date::get_day(const Date &object)
 {
-  return object.index_temperature;
+  return object.day;
 }
 ostream& operator<< (ostream &out, const Date &object)
 {
